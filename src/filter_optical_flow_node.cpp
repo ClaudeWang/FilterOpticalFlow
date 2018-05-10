1c090e0c53839c555b30f641ddcd0e812c0c7d84
yibo <hkualbert@gmail.com>
2018-05-10 14:18:47 -0400

    clean up code of ros wrap

--- src/filter_optical_flow_node.cpp
+++ src/filter_optical_flow_node.cpp
@@ -9,13 +9,4 @@ int main(int argc, char** argv){
     filter_optical_flow::EstimateVelocity estimator(nh, nh_priv);
 
     ros::spin();
-//     std::tuple<MatrixXf, MatrixXf, MatrixXf> result;
-//     result = a.velocityFromFlow(flow, K, depth, pos);
-//     MatrixXf X, A, b;
-//     std::tie(X, A, b) = result;
-// //    std::cout << X << std::endl;
-//     std::cout << "Ransac started" <<std::endl;
-//     std::vector<int> inlier = a.RANSAC(100,0.3,flow,K,depth, pos);
-//     std::cout << "Ransac Finished" << std::endl;
-//     return 0;
 }
